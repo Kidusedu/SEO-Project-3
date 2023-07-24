85e8658831ea490df54c90d6ca251b180e48d819
GandhiHarsh2003 <23gandhiharsh@gmail.com>
2023-07-24 17:09:06 -0400

    newAppBranch

--- index copy.js
+++ index copy.js
@@ -1,7 +1,40 @@
-import { APIKEY, apikey, openAiKey } from './apikey.js';
+// require('dotenv').config();
+// const express = require('express');
+// const { Configuration, OpenAIApi } = require('openai');
+
+// const app = express();
+// app.use(express.json());
+
+// const configuration = new Configuration({
+//     apiKey: process.env.API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+// app.post('/api/chat', async (req, res) => {
+//     const prompt = req.body.prompt;
+//     try {
+//         const response = await openai.createChatCompletion({
+//             model: "gpt-3.5-turbo",
+//             messages: [{ role: "user", content: prompt }],
+//         });
+
+//         res.json(response.data);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({ error: 'An error occurred while processing your request.' });
+//     }
+// });
+
+import { APIKEY, apikey } from './apikey.js';
 
 var overallResume = ""
 document.addEventListener("DOMContentLoaded", function () {
+  const closeButton = document.getElementById("close");
+  closeButton.addEventListener("click", function () {
+    chrome.extension.getViews({ type: "popup" })[0].close();
+  });
+
   const url = "https://docwire-doctotext.p.rapidapi.com/extract_text";
   const doc = document.getElementById("resumeFile");
   doc.onchange = async () => {
@@ -100,18 +133,11 @@ async function generateCoverLetter(jobDescription, qualifications, responsibilit
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${openAiKey}`, // Replace 'openAiKey' with your actual OpenAI API key
+      Authorization:
+        "Bearer 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", // Replace with your OpenAI API key
     },
     body: JSON.stringify(requestBody),
   });
 
   return response.json();
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    var closeButton = document.getElementById("close");
-  
-    closeButton.addEventListener("click", function() {
-      chrome.extension.getViews({ type: "popup" })[0].close();
-    });
-  });
